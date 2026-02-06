0f8420b18fe5f40a67730d4c109544a80197b092
Kingiol <kingxiaokang@gmail.com>
2026-02-06 15:04:36 +0800

    ...

--- packages/config/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/index.ts', 'src/schema.ts'],
-  format: ['esm'],
-  dts: true,
-  sourcemap: true,
-  clean: true
-});
